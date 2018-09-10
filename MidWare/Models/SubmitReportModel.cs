fdb05f83c3b5592fcecc1f6ae9c88f0ff7ca8ca0
mridulsri <mridul81@gmail.com>
2018-09-10 14:27:01 +0530

    added

---
+++ MidWare/Models/SubmitReportModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MidWare.Models
+{
+    public class SubmitReportModel
+    {
+        public SubmitReportModel()
+        {
+            ProjectFeedList = new List<ProjectFeedModel>();
+        }
+
+        public ProjectFeedModel ProjectFeed { get; set; }
+        public List<ProjectFeedModel> ProjectFeedList { get; set; }
+        public AddReport AddReport { get; set; }
+        public AddInvoice AddInvoice { get; set; }
+    }
+
+    public class AddReport
+    {
+       
+        public string ProjectFeedId { get; set; }
+
+        [Required]
+        public string Report { get; set; }
+    }
+
+    public class AddInvoice
+    {
+        public string ProjectFeedId { get; set; }
+
+        [Required]
+        public string Invoice { get; set; }
+
+        
+    }
+}
